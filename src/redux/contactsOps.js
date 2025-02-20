33f9965e9909258ec0b12e086d2a5a50c90ac49e
Valeriia Martynova <vmartynova95@gmail.com>
2025-02-20 21:46:29 +0200

    create project

--- src/redux/contactsOps.js
+++ src/redux/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://67b22da7bc0165def8ccee3e.mockapi.io";
+axios.defaults.baseURL = "https://connections-api.goit.global/";
 
 const fetchContacts = createAsyncThunk(
   "contacts/fetchContacts",
