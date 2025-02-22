d6ec8244ff9ae2c4b916d1e81abc958aae58cd72
Valeriia Martynova <vmartynova95@gmail.com>
2025-02-22 23:47:54 +0200

    update ContactsPage

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import SearchBox from "../../components/SearchBox/SearchBox";
 import ContactList from "../../components/ContactList/ContactList";
-import { fetchContacts } from "../../redux/contactsOps";
-import { selectIsLoading } from "../../redux/selectors";
+import { fetchContacts } from "../../redux/contacts/operations";
+import { selectIsLoading } from "../../redux/contacts/selectors";
 
 const ContactsPage = () => {
   const dispatch = useDispatch();
